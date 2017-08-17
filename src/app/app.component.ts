8fa0901dfa780aef426482f9a3febe688e5e56ee
Walter Jr <walbinjr@gmail.com>
2017-08-17 09:40:44 -0300

    Ajuste no layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showSettings = false;
+
+  toggleSettings(event) {
+    this.showSettings = !this.showSettings;
+  }
 }
