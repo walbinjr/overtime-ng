2a256f71fdf388d745d12113fdead3001b2ef464
Walter Jr <walbinjr@gmail.com>
2017-08-17 09:40:52 -0300

    Adicionando localstorage, melhorando layout e adicionando mascara

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,47 @@
 import { Component } from '@angular/core';
 
+import { Settings } from './settings';
+import { SettingsDataService } from './settings-data.service';
+
+import * as moment from 'moment';
+
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [SettingsDataService]
 })
 export class AppComponent {
-  title = 'app';
   showSettings = false;
+  settings: Settings = new Settings();
+  arrivedTime: moment.Moment = moment();
+
+  constructor(private settingsDataService: SettingsDataService) {
+    this.settings = this.settingsDataService.getSettings();
+    if(this.settings.arrivedTime)
+      this.arrivedTime = moment(this.settings.arrivedTime);
+  }
 
   toggleSettings(event) {
     this.showSettings = !this.showSettings;
+    if(!this.showSettings) {
+      this.saveSettings();
+    }
+  }
+
+  saveArrivedTime() {
+    this.settings.arrivedTime = moment(this.arrivedTime).valueOf();
+    this.saveSettings();
+  }
+
+  resetSettings() {
+    this.settings = new Settings();
+    this.saveSettings();
+  }
+
+  saveSettings() {
+    this.settingsDataService.saveSettings(this.settings);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+
+import { LocalStorageModule } from 'angular-2-local-storage';
+import { NgxMaskModule } from 'ngx-mask';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    LocalStorageModule.withConfig({
+      prefix: 'app',
+      storageType: 'localStorage'
+    }),
+    NgxMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
